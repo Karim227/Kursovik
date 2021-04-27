0ae7ea9a08c99a1827583eeaf56f3918a15c3c0e
Карим Габибов <Gabibov_KZ@eduportal44.ru>
2021-04-27 20:24:38 +0300

    DRUG

---
+++ Kursovik/Kursovik/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Kursovik.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kursovik.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UsersContext db;
+        public HomeController(UsersContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            //return Content(User.Identity.Name);
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Messeng(Messange mess)
+        {
+            if (mess != null)
+            {
+                try
+                {
+                    db.Messengs.Add(mess);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    ViewBag.Error = "Ошибка отправки сообщения";
+                    return RedirectToAction("Index");
+                }
+            }
+            ViewBag.Error = "Ошибка отправки сообщения";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Kursovik/Kursovik/SampleData.cs
@@ -0,0 +1,31 @@
+﻿using Kursovik.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kursovik
+{
+    public class SampleData
+    {
+        public static void Initialize(UsersContext context)
+        {
+            if (!context.Users.Any())
+            {
+                context.Users.AddRange(
+                    new User
+                    {
+                        Email = "Admin",
+                        Password = "AdminBTGP",
+                    },
+                    new User
+                    {
+                        Email = "MoDeR",
+                        Password = "ModerBTGP",
+                    });
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
